30b667059fb85a347b82db91fa94e2da336cf622
brunobrg <bbraganca1991@gmail.com>
2017-05-26 11:59:05 -0300

    detecting faces in each frame without delay

--- teia/videostream.py
+++ teia/videostream.py
@@ -24,10 +24,20 @@ class VideoStream(QtCore.QThread):
         while self.streaming:
             self.grabbed, self.frame = self.stream.read()
 
-            img = cv2.resize(self.frame, (self.window_width, self.window_height), interpolation=cv2.INTER_CUBIC)
+            small_frame = cv2.resize(self.frame, (0, 0), fx=0.25, fy=0.25)
+            face_locations = face_recognition.face_locations(small_frame)
 
-            if self.target != None:
-                img = self.facereq(img)
+            for (top, right, bottom, left) in (face_locations):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 255, 0), 1)
+
+            img = cv2.resize(self.frame, (self.window_width, self.window_height), interpolation=cv2.INTER_CUBIC)
 
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             height, width, bpc = img.shape
@@ -47,27 +57,8 @@ class VideoStream(QtCore.QThread):
         self.target = face_recognition.face_encodings(image)[0]
 
 
-    def facereq(self, frame):
-        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        face_locations = []
-        face_names = []
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(small_frame)
-        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
-
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([self.target], face_encoding)
-            name = "Desconhecido"
-
-            if match[0]:
-                name = "Alvo encontrado"
-
-            face_names.append(name)
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
+    def facereq(self, face_locations):
+        for (top, right, bottom, left) in (face_locations):
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
             top *= 2
             right *= 2
@@ -75,11 +66,6 @@ class VideoStream(QtCore.QThread):
             left *= 2
 
             # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 1)
 
         return frame
