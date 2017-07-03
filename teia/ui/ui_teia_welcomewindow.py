e621ca5361d5ecc6e4b2fe7070bea013bf88f501
anacarolina1 <ana_karolopes@hotmail.com>
2017-07-02 23:50:47 -0300

    Add project image to welcome window.

--- teia/ui/ui_teia_welcomewindow.py
+++ teia/ui/ui_teia_welcomewindow.py
@@ -7,31 +7,23 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-#from PyQt5.QtGui import QApplication
-
 
 class Ui_WelcomeWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(468, 254)
+        MainWindow.resize(599, 342)
         self.layoutWidget = QtWidgets.QWidget(MainWindow)
         self.layoutWidget.setObjectName("layoutWidget")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.layoutWidget)
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem, 5, 0, 1, 1)
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem1, 1, 0, 1, 1)
+        self.image = QtWidgets.QLabel(self.layoutWidget)
+        self.image.setObjectName("image")
+        self.gridLayout.addWidget(self.image, 0, 0, 1, 1)
         self.enterButton = QtWidgets.QPushButton(self.layoutWidget)
         self.enterButton.setObjectName("enterButton")
-        self.gridLayout.addWidget(self.enterButton, 3, 0, 1, 1)
-        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem2, 3, 0, 1, 1)
-        self.labelName = QtWidgets.QLabel(self.layoutWidget)
-        self.labelName.setObjectName("labelName")
-        self.gridLayout.addWidget(self.labelName, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.enterButton, 1, 0, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.layoutWidget)
 
@@ -41,7 +33,7 @@ class Ui_WelcomeWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("WelcomeWindow", "Form"))
+        self.image.setText(_translate("WelcomeWindow", "<html><head/><body><p align=\"center\"><img src=\":/newPrefix/teia.png\"/></p></body></html>"))
         self.enterButton.setText(_translate("WelcomeWindow", "Entrar"))
-        self.labelName.setText(_translate("WelcomeWindow", "                                     Bem Vindo, ao Projeto Téia!"))
 
-
+from teia.design import image
